c5bda41f38a4bee66e646bc1bbe30b7b9ccfe579
rreavennnn <reavenputra0@gmail.com>
2024-01-11 08:42:12 +0700

    pphpmvc

---
+++ app/controllers/Siswa.php
@@ -0,0 +1,11 @@
+<?php
+class Siswa{
+
+    public function index(){
+        echo"ini adalah siswa index";
+    }
+    public function biodata (){
+        echo"inia adalah siswa/biodata";
+
+    }
+}
